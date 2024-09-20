726aeb8147e1181e0d8c862c38414c6f3ea80c9a
Dmitrii Timofeev <28dngn@gmail.ru>
2024-09-20 23:14:01 +0300

    задание 7-converter исправлено

--- 7-converter/7-converter.js
+++ 7-converter/7-converter.js
@@ -1,45 +1,86 @@
-function getExchangeRate(originalCarrency, targetCarrency) {
-    const original = originalCarrency.toUpperCase();
-    const target = targetCarrency.toUpperCase();
+const getTargetCurrencyIcon = (target) => {
+    target = target.toUpperCase();
 
-    switch(true) {
-        case original === 'RUB' && target === 'USD':
-            return 91.11;
-        case original === 'RUB' && target === 'EUR':
-            return 100.5;
-        case original === 'RUB' && target === 'CNY':
-            return 12.7;
-        case original === 'USD' && target === 'RUB':
-            return 0.010975;
-        case original === 'USD' && target === 'EUR':
-            return 0.90646;
-        case original === 'USD' && target === 'CNY':
-            return 0.140428;
-        case original === 'EUR' && target === 'RUB':
-            return 0.00995;
-        case original === 'EUR' && target === 'USD':
-            return 0.90646;
-        case original === 'EUR' && target === 'CNY':
-            return 0.90646;
-        case original === 'CNY' && target === 'RUB':
-            return 0.07877;
-        case original === 'CNY' && target === 'USD':
-            return 7.12;
-        case original === 'CNY' && target === 'EUR':
-            return 7.85;
+    switch (target) {
+        case 'RUB':
+            return 'руб';
+        case 'USD':
+            return '$';
+        case 'EUR':
+            return '€';
+        case 'CNY':
+            return '¥';
+        default:
+            return '';
+    }
+}
+
+const convertSum = (sum, initial, target) => {
+    const USD = 90;
+    const EUR = 100;
+    const CNY = 12;
+
+    initial = initial.toUpperCase();
+    target = target.toUpperCase();
+
+    switch (initial) {
+        case 'CNY':
+            switch (target) {
+                case 'RUB':
+                    return sum * CNY;
+                case 'USD':
+                    return (sum * CNY) / USD;
+                case 'EUR':
+                    return (sum * CNY) / EUR;
+                default:
+                    return null;
+            }
+        case 'USD':
+            switch (target) {
+                case 'RUB':
+                    return sum * USD;
+                case 'EUR':
+                    return (sum * USD) / EUR;
+                case 'CNY':
+                    return (sum * USD) / CNY;
+                default:
+                    return null;
+            }
+        case 'EUR':
+            switch (target) {
+                case 'RUB':
+                    return sum * EUR;
+                case 'USD':
+                    return (sum * EUR) / USD;
+                case 'CNY':
+                    return (sum * EUR) / CNY;
+                default:
+                    return null;
+            }
+        case 'RUB':
+            switch (target) {
+                case 'USD':
+                    return sum / USD;
+                case 'EUR':
+                    return sum / EUR;
+                case 'CNY':
+                    return sum / CNY;
+                default:
+                    return null;
+            }
         default:
             return null;
     }
 }
 
-// ПОДДЕРЖИВАЕМЫЕ ВАЛЮТЫ: RUB, USD, EUR, CNY
+const converter = (sum, currency, targetCurrency, func) => {
+    let resSum = func(sum, currency, targetCurrency);
+    resSum = resSum ? resSum.toFixed(2) : resSum;
 
-function getConversion(sum, originalCarrency, targetCarrency) {
-    if (getExchangeRate(originalCarrency, targetCarrency)) {
-        const res = (sum / getExchangeRate(originalCarrency, targetCarrency)).toFixed(2);
+    const icon = resSum ? getTargetCurrencyIcon(targetCurrency) : '';
 
-        return `Сумма конвертации ${sum} ${originalCarrency.toUpperCase()} в ${targetCarrency.toUpperCase()} равна ${res} ${targetCarrency.toUpperCase()}`;
-    }
-    
-    return `Конвертация данной пары валют не поддерживается`;
+    return `${resSum} ${icon}`;
 }
+
+console.log(converter(1000, 'rub', 'cny', convertSum)); // 83.33 ¥;
+console.log(converter(1000, 'rub', 'chf', convertSum)); // null;
