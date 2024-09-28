d504b9456ea64c0b87e10da89ae55cd4964ab970
Dmitrii Timofeev <28dngn@gmail.ru>
2024-09-28 18:14:30 +0300

    fix 13-query

--- 13-query/13-query.js
+++ 13-query/13-query.js
@@ -1,10 +1,14 @@
-const getQueryString = (data) => {
-    let str = '';
+const getQueryString1 = (data) => {
+    if (!data) {
+        console.log('Отсутствуют данные при вызове функции');
+        return null;
+    }
 
+    let str = '';
     for (let key in data) {
         str += `${key}=${data[key]}&`;
     }
     return str.slice(0, -1);
-}
+};
 
-console.log(getQueryString({search: "Вася", take: 10}));
+console.log(getQueryString1({search: "Вася", take: 10}));
